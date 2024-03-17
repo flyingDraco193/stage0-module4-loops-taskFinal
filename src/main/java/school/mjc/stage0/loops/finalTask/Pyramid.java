c41b3b096fe34712a35b232efc8cd9c9b7f641b0
Gempton <gemptonilya@gmail.com>
2024-03-17 10:09:54 +0300

    Done

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,26 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        int height = cathetusLength * 2 - 1;
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= height; j++) {
+                if (j == cathetusLength) {
+                    System.out.print(1);
+                } else if (j >= cathetusLength - i + 1 && j <= cathetusLength + i - 1) {
+                    if (j >= cathetusLength - i + 1 && j < cathetusLength) {
+                        System.out.print(cathetusLength - j + 1);
+                    } else {
+                        System.out.print(j - cathetusLength + 1);
+                    }
+                } else if (j < cathetusLength) {
+                    System.out.print(" ");
+                }
 
+                if (j == height) {
+                    System.out.println();
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
